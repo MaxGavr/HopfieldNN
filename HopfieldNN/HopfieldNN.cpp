cb00cb2119d367140f947a9795eecb87b63746e8
Max Gavrilyuk <max.gavr@tut.by>
2017-12-25 00:49:40 +0300

    Refactor code

--- HopfieldNN/HopfieldNN.cpp
+++ HopfieldNN/HopfieldNN.cpp
@@ -1,168 +1,210 @@
-﻿// HopfieldNN.cpp: определяет точку входа для консольного приложения.
-//
-
-#include <iomanip>
+﻿#include <iomanip>
 #include <vector>
 #include <string>
 #include <fstream>
 #include <iostream>
-// 2003-2010 Free Software Foundation Inc.
+#include <algorithm>
 #include <ctime>
-//1997-2015 Free Software Foundation, Inc.
-
-/*
-14.12.2017 автор - Ника Ковальчук
-*/
 
 using namespace std;
 
-/* Глобальные переменные
-*  letters - хранит алфавит в виде строки;
-*  parsedLetters - хранит алфавит в виде чисел (1 и -1)
-*  13.12.2017 - автор: Ника Ковальчук.
-*/
-vector<string> letters;
-vector<vector<int>> parsedLetters;
-string testLetter = "...#.#.##";
 
-/* Функция для вычисления перевода символов в числа
-*  Составляет вектор из значений входного вектора, заменив их в 1 (для #) или -1(для .).
-*  13.12.2017 - автор: Ника Ковальчук.
-*/
-vector<int> parseFromString(string letter) {
-	vector<int> result;
-	for (int i = 0; i < letter.size(); ++i) {
-		switch (letter[i]) {
-		case '.': {
-			result.push_back(-1);
-			break;
-		}
-		case '#': {
-			result.push_back(1);
-			break;
-		}
-		}
-	}
+vector<string> symbols;
+vector<vector<int>> parsedSymbols;
+string testSymbol = "...#.#.##";
+
+vector<int> parseFromString(string letter)
+{
+    letter.erase(remove_if(letter.begin(), letter.end(), isspace), letter.end());
+    vector<int> result(letter.size());
+
+    auto toInt = [](char c) -> int
+    {
+        if (c == '.')
+            return -1;
+        else if (c == '#')
+            return 1;
+        else
+            throw logic_error("Неверное значение входного вектора!");
+    };
+
+    transform(letter.begin(), letter.end(), result.begin(), toInt);
+
 	return result;
 }
 
-int main() {
+int main()
+{
 	setlocale(LC_ALL, "");
 
 	int WIDTH = 9;
 	int m = 0;
 	int step = 0;
 
-	cout << "Лабораторная работа №3. \nРелаксационные нейронные сети.\nРаспознавание символов сетью Хопфилда.\n\n";
-	cout << "Какой размер образа? (пользовательский (необохимо ввести алфавит) или системный (9))\n\n";
-	cout << "1. Пользовательский.\n2. Системный.\n";
+	cout << "Сеть Хопфилда.\n\n";
+    cout << "Введите размер входного образа:" << endl;
+    cout << "1. Произвольный" << endl;
+    cout << "2. По умолчанию (9)" << endl;
+
 	int choose;
 	cin >> choose;
 	cout << "Алфавит образов: \n";
-	if (choose == 2) {
+	if (choose == 2)
+    {
 		srand(time(NULL));
-		for (char letter = 'a'; letter < 'c'; letter++) { //j
+		for (char letter = 'a'; letter < 'c'; letter++)
+        {
 			string filename = "x.txt";
 			filename[0] = letter;
+
 			ifstream fileStream(filename);
 			string representation(istreambuf_iterator<char>{fileStream}, {});
 			fileStream.close();
-			letters.push_back(representation);
-			parsedLetters.push_back(parseFromString(representation));
-			cout << m + 1 << ". " << letters[m] << endl;
+
+			symbols.push_back(representation);
+			parsedSymbols.push_back(parseFromString(representation));
+
+			cout << m + 1 << ". " << symbols[m] << endl;
 			m++;
 		}
+
 		cout << "\nВвести образ самостоятельно или воспользователься системным?\n1.Свой;\n2.Системный:\n";
-		cout << testLetter << endl;
+		cout << testSymbol << endl;
 		cin >> choose;
-		if (choose ==1){
+		if (choose ==1)
+        {
 			cout << "Введите " << WIDTH << " символов :\n";
 			string temp;
 			cin >> temp;
-			testLetter += temp;
+
+			testSymbol += temp;
 		}
 	}
-	else {
+	else
+    {
 		int number;
 		cout << "Введите ширину символов : \n";
 		cin >> WIDTH;
 		cout << "Введите число символов в афавите : \n";
 		cin >> number;
+
 		string sign;
-		for (int i = 0; i < number; i++) {
+		for (int i = 0; i < number; i++)
+        {
 			cout << "Введите символ алфавита: \n";
 			cout << "Введите " << WIDTH << "символов :\n";
 			string temp;
 			cin >> temp;
 			sign += temp;
-			letters.push_back(sign);
-			parsedLetters.push_back(parseFromString(sign));
+
+			symbols.push_back(sign);
+			parsedSymbols.push_back(parseFromString(sign));
+
 			m++;
 		}
 		cout << "Введите обрабатываемый образ " << WIDTH << " символов :\n";
 		string temp;
 		cin >> temp;
-		testLetter += temp;
+
+		testSymbol += temp;
 	}
 
 	cout << "\nПросматривать выход нейрона сетив пошаговом режиме? (1-да, 2-нет.) :\n";
 	cin >> step;
 	cout << endl;
-	vector<int> Y;
+
 	vector<vector<int>> relax; // нужно чтоб понять достигла ли сеть релаксации или нет (по заданию не требуется)
 	bool relaxBool = false;
-	vector<vector<double>> W;
-	vector<double> temp;
-	int result = -1;
-	if (step == 1) cout << "Весовая матрица: \n";
-	for (int i = 0; i < WIDTH; i++) {
-		W.push_back(temp);
-		for (int j = 0; j < WIDTH; j++) {
-			int sum = 0;
-			for (int k = 0; k < m; k++) {
-				sum += parsedLetters[k][j] * parsedLetters[k][i];
-			}
-			if (i == j) sum = 0;
-			W[i].push_back(sum);
-			cout.precision(2);
-			if (step == 1) cout << W[i][j] << " ";
+
+    // initialize weight matrix
+    vector<vector<double>> W(WIDTH, vector<double>(WIDTH, 0));
+
+	for (int i = 0; i < WIDTH; i++)
+    {
+		for (int j = 0; j < WIDTH; j++)
+        {
+            if (i == j)
+                continue;
+
+			for (int k = 0; k < m; k++)
+				W[i][j] += parsedSymbols[k][j] * parsedSymbols[k][i];
 		}
-		if (step == 1) cout << endl;
 	}
-	if (step == 1)  cout << endl;
-	Y = parseFromString(testLetter);
+
+    // print out weight matrix
+    if (step == 1)
+    {
+        cout << "Весовая матрица: \n";
+
+        for (const vector<double>& row : W)
+        {
+            for (double value : row)
+                cout << setprecision(2) << value << " ";
+            cout << endl;
+        }
+
+        system("pause");
+        cout << endl;
+    }
+
+    vector<int> Y = parseFromString(testSymbol);
+
+    int result = -1;
+
 	bool cont = true;
-	int itr = 1;
+	int iteration = 1;
 	int neuron = -1;
-	while (cont && relaxBool != true) {
-		if (neuron < WIDTH-1) neuron++;
-		else neuron = 0;		
+	while (cont && !relaxBool)
+    {
+		if (neuron < WIDTH-1)
+            neuron++;
+		else
+            neuron = 0;
+
 		double sum = 0;
-		for (int i = 0; i < WIDTH; i++) {
+		for (int i = 0; i < WIDTH; i++)
 			sum += W[i][neuron] * Y[neuron];
+
+		if (sum > 0)
+            Y[neuron] = 1;
+		else
+            Y[neuron] = -1;
+
+		for (int i = 0; i < relax.size(); i++)
+        {
+			if (relax[i] == Y && neuron == WIDTH)
+                relaxBool = true;
 		}
-		if (sum > 0) Y[neuron] = 1;
-		else Y[neuron] = -1;
-		for (int i = 0; i < relax.size(); i++) {
-			if (relax[i] == Y && neuron == WIDTH ) relaxBool = true;
-		}
-		for (int i = 0; i < m; i++) {
-			if (Y == parsedLetters[i]) {
+
+		for (int i = 0; i < m; i++)
+        {
+			if (Y == parsedSymbols[i])
+            {
 				result = i;
 				cont = false;
 			}
 		}
-		itr++;
+
+		iteration++;
 		relax.push_back(Y);
-		cout << "\nИтерация " << itr << ": ";
-		for (int i = 0; i < WIDTH; i++) {
-			if (Y[i] == 1) cout << "# ";
-			else cout << ". ";
+
+		cout << "\nИтерация " << iteration << ": ";
+		for (int i = 0; i < WIDTH; i++)
+        {
+			if (Y[i] == 1)
+                cout << "# ";
+			else
+                cout << ". ";
 		}
-		if (neuron == WIDTH) cout << endl;
+		if (neuron == WIDTH)
+            cout << endl;
 	}
-	if (relaxBool == true) cout << "\n\nСеть достигла релаксации.\n";
-	if (result != -1 )cout << "Введенный образ соответствует образу " << 1 + result << " :" << letters[result] << endl;
-	system("pause");
+
+	if (relaxBool == true)
+        cout << "\n\nСеть достигла релаксации.\n";
+
+	if (result != -1 )
+        cout << "Введенный образ соответствует образу " << 1 + result << " :" << symbols[result] << endl;
+
 	return 0;
 }
